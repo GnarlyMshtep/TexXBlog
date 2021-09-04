4598e244011c12b691dc05d36bb1d0aa2e7f8222
Matan Shtepel <46758065+GnarlyMshtep@users.noreply.github.com>
2021-09-04 14:47:07 -0700

    styling semi-complete, move to js

--- frontend/main.js
+++ frontend/main.js
@@ -1,25 +1,65 @@
-// Selecting the iframe element
+//constants
+const maxTitleLength = 20;
+const maxAuthorLength = 15;
 
+/**
+ * @abstract set the iframe's proper height
+ */
 const frame = document.querySelector("Iframe");
-//const sidebar = document.querySelector("nav")
-console.log(frame);
-
-
-
-// Adjusting the iframe height onload event
-frame.onload = function ()
-// function execute while load the iframe
-{
-    console.log('hello!');
-    // set the height of the iframe as 
-    // the height of the iframe content
+frame.onload = () => {
     frame.style.height =
         frame.contentWindow.document.body.scrollHeight + 'px';
 
-
-    // set the width of the iframe as the 
-    // width of the iframe content
     frame.style.width =
         frame.contentWindow.document.body.scrollWidth + 'px';
 
+}
+
+/**
+ * @abstract populate the navbar with the appropriate articles
+ */
+const navContentContainer = document.querySelector('.nav-content-container');
+const populateNavbar = (filter) => {
+    //first, empty the navbar.
+    navContentContainer.innerHTML = "";
+
+}
+
+/**
+ * 
+ * @param {CreateNavButton} title 
+ * @param {Matan SHtepel} author 
+ * @param {9/4/2021} date 
+ * @returns html eleent such as: 
+ *          <div class="article-button nav-button">
+                    <p class="article-button-title">Merkle Trees: Fundementals</p>
+                    <p class="article-button-author">Ishaan Dham</p>
+                    <p class="article-button-date">9/4/2021</p>
+            </div>
+ */
+const createNavButtonHTML = (title, author, date, ) => {
+    //normalize text length
+    title = title.length > maxTitleLength ? title.substr(0, maxTitleLength) + "..." : title;
+    author = author.length > maxAuthorLength ? author.substr(0, maxAuthorLength) + "..." : author;
+
+    //set up html elements
+    let button = document.createElement("div");
+
+    let titleP = document.createElement("p")
+    titleP.innerText = title;
+    titleP.className = "article-button-title"
+
+    let authorP = document.createElement("p");
+    authorP.innerText = author;
+    titleP.className = "article-button-author"
+
+    let dateP = document.createElement("p");
+    dateP.innerText = date;
+    titleP.className = "article-button-date"
+
+    button.appendChild(titleP);
+    button.appendChild(authorP);
+    button.appendChild(dateP);
+
+    return button;
 }
