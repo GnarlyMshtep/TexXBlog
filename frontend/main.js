67c9846353a9deb45f5e266de35ae0969b56876b
Ishaan Dham <ishaandham01@gmail.com>
2022-01-13 04:40:14 -0500

    refactor js files into main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,22 @@
-import utility from './utility.js'
+// Variables
+var posts = []
+var keyWords = []
 
-// window variables 
-window.posts = [];
+// Constants
+const MAX_TITLE_LENGTH = 20;
+const MAX_AUTHOR_LENGTH = 15;
+const START_ONSLIDE_INTO_SCREEN = 80
+const NUM_KEYWORDS = 30
 
+/**
+ * Function obtains the json data from config.json and set's up the entire page.
+ */
 async function main() {
     const filters = [];
     const jsonData = await loadJSONData(); //!start by loading JSON, we can do that concurrently with page load -- this is nice : )
 
 
-    //?create an event listener for the loaded function -- we had a problem that sometime the window would be loaded before we got here
+    // create an event listener for the loaded function (had a problem that sometime the window would be loaded before we got here)
     if (document.readyState === 'complete') {
         windowLoaded();
     } else {
@@ -16,7 +24,7 @@ async function main() {
     }
 
     /**
-     * @abstract Make the injections from teh JSON file to make the page customizable + set up event listeners. 
+     * Make injections from the JSON file to make the page customizable + set up event listeners. 
      */
     function windowLoaded() {
         // read query string and update filters
@@ -29,14 +37,13 @@ async function main() {
         adjustFrameHeight(Iframe);
 
         // create global variable array of posts (used in filter process)
-        window.posts = jsonData.posts;
+        posts = jsonData.posts;
 
         //fix the colors
         fixColors(jsonData.colors);
 
         //nav content container for population
-        const navContentContainer = document.querySelector('.nav-content-container');
-        populateNavbar(jsonData.posts, filters, navContentContainer);
+        populateNavbar(jsonData.posts);
 
         //fill in header links
         const headerLinksContainer = document.querySelector('.header-links');
@@ -65,10 +72,12 @@ async function main() {
 
 
 
-        //!attach event listeners
+        //! Attach event listeners
 
-        //for click on a different article, changes the Iframe src
+        // for click on a different article, changes the Iframe src
+        const navContentContainer = document.querySelector('.nav-content-container');
         navContentContainer.addEventListener('click', (clicked) => {
+            // path doesn't work in safari and firefox.
             var _path = clicked.path || (clicked.composedPath && clicked.composedPath());
             if (_path[1].id) {
                 Iframe.src = "../_docs/" + jsonData.posts[_path[1].id].filename;
@@ -96,7 +105,10 @@ async function main() {
     }
 }
 
-
+//! Remove this function.
+/**
+ * Function loads the latest blog in the iframe.
+ */
 function loadLastBlogPost(jsonData) {
     document.getElementById('Iframe').src = '../_docs/' + jsonData.posts[jsonData.posts.length - 1].filename
 }
@@ -113,7 +125,7 @@ function scrollNavBar(headerWrapper, navToContentSeperator, isScrollOut, origina
 
     if (isScrollOut) {
         if (this.offsetWidth > 2) {
-            temp = utility.sideBarWidth(originalWidth, currentCall, isScrollOut)
+            temp = sideBarWidth(originalWidth, currentCall, isScrollOut)
             this.style.width = temp + "px";
             navToContentSeperator.style.marginLeft = temp + "px"
             headerWrapper.style.marginLeft = temp + "px"
@@ -123,7 +135,7 @@ function scrollNavBar(headerWrapper, navToContentSeperator, isScrollOut, origina
         }
     } else {
         if (this.offsetWidth < originalWidth - 1.5) {
-            temp = utility.sideBarWidth(originalWidth, currentCall, isScrollOut)
+            temp = sideBarWidth(originalWidth, currentCall, isScrollOut)
             navToContentSeperator.style.marginLeft = temp + "px"
             this.style.width = temp + "px";
             headerWrapper.style.marginLeft = temp + "px"
@@ -135,16 +147,8 @@ function scrollNavBar(headerWrapper, navToContentSeperator, isScrollOut, origina
 
 }
 
-//utilities end
-//constants
-//!make constants for the class names of the created elements
-
-
-
-//constants end
-
 /**
- * @abstract set the iframe's proper height
+ * Set the iframe's proper height
  */
 
 function adjustFrameHeight(Iframe) {
@@ -154,9 +158,10 @@ function adjustFrameHeight(Iframe) {
 }
 
 /**
+ * Function add the proper color scheme from config.json file
  * @param {json} colors taken from jsonData
  */
-const fixColors = async (colors) => {
+const fixColors = (colors) => {
     // Json loaded
     if (colors) {
         const r = document.querySelector(':root');
@@ -169,30 +174,28 @@ const fixColors = async (colors) => {
 }
 
 /**
- * @abstract populate the navbar with the appropriate articles
+ * Populate the navbar with the appropriate articles
  *
- * 
  * @param {json} posts Comes from jsonData
- * @param {the filter of the posts} filter 
+ * @param {String[]} filter the filter of the posts
  */
-const populateNavbar = async (posts, filters = [], navContentContainer) => {
-    // JSON loaded
-    //then, empty the navbar. we onload so we can load the JSON before the entire file loads
+const populateNavbar = (posts, filter = null) => {
+    // Empty the navbar. we onload so we can load the JSON before the entire file loads
+    const navContentContainer = document.querySelector('.nav-content-container');
     navContentContainer.innerHTML = "";
 
     let putUpArticles = false; //we need this know if we should place the empty message
     if (posts) {
-        for (let i = 0; i < posts.length; i++) {
-            //!check the filter
-            if (utility.tagsMatch(posts[i].tags, filters)) {
+        // Add the filtered posts
+        for (const post of posts) {
+            if (filter == null || filter.has(post.id)) {
                 putUpArticles = true
-                navContentContainer.appendChild(utility.createNavButtonHTML(posts[i].title, posts[i].author, posts[i].date, posts[i].id))
+                navContentContainer.appendChild(createNavButtonHTML(post.title, post.author, post.date, post.id))
             }
-
         }
-
     }
-    if (!putUpArticles || !posts) { //in case there were no posts matching the tags, we want to let the user know
+    // case there were no posts matching the tags, we want to let the user know
+    if (!putUpArticles || !posts) {
         const noPostsP = document.createElement("p");
         noPostsP.className = "no-posts-p "
         noPostsP.innerText = "No posts available with the applied filters. Try different filters!"
@@ -200,10 +203,16 @@ const populateNavbar = async (posts, filters = [], navContentContainer) => {
     }
 
 }
+
+/**
+ * Function adds header links to navbar
+ * @param {String[]} links 
+ * @param {*} headerLinksContainer 
+ */
 const populateHeaderLinks = (links, headerLinksContainer) => {
     if (links) {
         for (let i = 0; i < links.length; i++) {
-            headerLinksContainer.appendChild(utility.createNavLinkButtonHTML(links[i]))
+            headerLinksContainer.appendChild(createNavLinkButtonHTML(links[i]))
         }
     }
 }
@@ -236,25 +245,183 @@ const readQueryString = () => {
 }
 
 /**
- * 
- * @param {CreateNavButton} title 
- * @param {Matan SHtepel} author 
- * @param {9/4/2021} date 
- * @returns html eleent such as: 
- *          <div class="article-button nav-button">
-                    <p class="article-button-title">Merkle Trees: Fundementals</p>
-                    <p class="article-button-author">Ishaan Dham</p>
-                    <p class="article-button-date">9/4/2021</p>
-            </div>
+ * Function fetches and converts config.json into a object.
+ * @returns object
  */
-
 const loadJSONData = async () => {
     const rawJson = await fetch('../config.json');
-    /*const reader = rawJson.arrayBuffer.body.getReader()
-    console.log(reader);*/
     const jsonData = await rawJson.json();
     return jsonData
 }
 
+// Call main
 main();
-//!always handle events last
+
+//! Utility Functions. 
+
+/**
+ * Calculates and returns the sidebar width.
+ * @param {*} originalWidth 
+ * @param {*} callNumber 
+ * @param {*} isForward 
+ * @returns int
+ */
+function sideBarWidth(originalWidth, callNumber, isForward) {
+    callNumber = isForward ? callNumber : START_ONSLIDE_INTO_SCREEN - callNumber
+
+    /*return (-originalWidth / 4) * (.05 * callNumber - 6) ** (1 / 3) + (1 / 2) * originalWidth*/
+    //console.log(-originalWidth / (1 + Math.exp(-.06 * (callNumber - 100))) + originalWidth);
+    return (-originalWidth / (1 + Math.exp(-.12 * (callNumber - 40))) + originalWidth + 2)
+
+}
+
+/**
+ * Creates a blog entry in the side navbar.
+ * @param {String} title 
+ * @param {String} author 
+ * @param {*} date 
+ * @param {*} id 
+ * @returns 
+ */
+const createNavButtonHTML = (title, author, date, id) => {
+    //normalize text length
+    title = title.length > MAX_TITLE_LENGTH ? title.substr(0, MAX_TITLE_LENGTH) + "..." : title;
+    author = author.length > MAX_AUTHOR_LENGTH ? author.substr(0, MAX_AUTHOR_LENGTH) + "..." : author;
+
+    //set up html elements
+    let button = document.createElement("div");
+    button.className = "article-button nav-button"
+    button.id = id
+
+    let titleP = document.createElement("p")
+    titleP.innerText = title;
+    titleP.className = "article-button-title"
+
+    let authorP = document.createElement("p");
+    authorP.innerText = author;
+    authorP.className = "article-button-author"
+
+    let dateP = document.createElement("p");
+    dateP.innerText = date.monthDayYear.toString().replaceAll(',', '/');
+    dateP.className = "article-button-date"
+
+    button.appendChild(titleP);
+    button.appendChild(authorP);
+    button.appendChild(dateP);
+
+    return button;
+}
+
+/**
+ * Creates a nav link.
+ * @param {String} link 
+ * @returns 
+ */
+const createNavLinkButtonHTML = (link) => {
+    const a = document.createElement("a")
+    a.href = link.href;
+
+    const div = document.createElement("div")
+    div.className = "header-link"
+    div.innerText = link.title
+
+    a.appendChild(div)
+    return a
+}
+
+/** 
+* Function that is used to toggle the display of the filter container.
+*
+* */
+function filterButtonToggle() {
+    filterContainer = document.getElementById("filter-container");
+    if (filterContainer.className == "hide") {
+        filterContainer.className = "show"
+    }
+    else {
+        filterContainer.className = "hide"
+    }
+}
+
+/**
+ * Adds the new keyword to variable keywords and re-populates the filter-container
+ * using populateFilterTags()
+ * 
+ * */
+function updateKeywords() {
+    let text = document.getElementById('filter-search-bar').value
+    if (text != "") {
+        // Limit the number of keywords to NUM_KEYWORDS.
+        if (keyWords.length <= NUM_KEYWORDS) {
+            keyWords.push(text.toLowerCase())
+        } else {
+            alert("No more keywords can be added")
+        }
+        populateFilterTags();
+    }
+    // clear the search bar
+    document.getElementById('filter-search-bar').value = "";
+}
+
+/**
+ * Populates div "tags-list" with all the tags in var keywords.
+ */
+function populateFilterTags() {
+    // remove all the tags
+    document.getElementById("tags-list").innerHTML = ""
+    // add all the tags
+    keyWords.forEach((tag) => {
+        let list = document.getElementById("tags-list");
+        // create a <li> with <span> for close 'X'
+        let node = document.createElement("li");
+        // limit display of tag to 10 chars
+        let tagNode = document.createTextNode(tag.substring(0, 11).toLowerCase());
+        let span = document.createElement('span');
+        span.innerHTML = "&times";
+        // Onclick function deletes the keyword and re-populates the tags.
+        span.onclick = () => {
+            let index = keyWords.indexOf(tag);
+            if (index != -1) {
+                keyWords.splice(index, 1);
+            }
+            populateFilterTags();
+        }
+
+        node.appendChild(tagNode);
+        node.appendChild(span);
+        list.appendChild(node);
+    })
+}
+
+/**
+ * Function that filters the posts using keywords and updates the blogs in 
+ * navbar by calling populateNavbar 
+ * of the filters posts
+ * @returns Set()
+ */
+const filterPosts = () => {
+    let filterPostsId = new Set();
+    if (posts) {
+        posts.forEach((post) => {
+            // keywords of post
+            postKeywords = new Set();
+            postKeywords.add(post.title.toLowerCase());
+            postKeywords.add(post.author.toLowerCase());
+            post.tags.forEach((tag) => {
+                postKeywords.add(tag.toLowerCase())
+            })
+            hasAllKeywords = true;
+            for (let i = 0; i < keyWords.length; i++) {
+                if (!postKeywords.has(keyWords[i])) {
+                    hasAllKeywords = false;
+                    break;
+                }
+            }
+            if (hasAllKeywords) {
+                filterPostsId.add(post.id);
+            }
+        })
+    }
+    // Update the navbar
+    populateNavbar(posts, filterPostsId);
+}
